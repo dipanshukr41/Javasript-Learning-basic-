7b905598eb0305437160c17efc7d98e4d736419b
Dipanshu Kumar <159459003+dipanshukr41@users.noreply.github.com>
2024-07-03 12:16:13 +0530

    Add files via upload

---
+++ day-0/script.js
@@ -0,0 +1,27 @@
+console.log("dipanshu")
+
+const accid =144555
+let accemail ="abcd.com"
+var accpassword = "123442"
+acccity = "jamshedpur"
+ //whenever you try to change const it will show error
+
+ accemail ="honeysingh@gmail.com"
+console.log(accemail); 
+// to multiple thing at same time just use console.table([])....
+
+
+console.table([accemail,accid,accpassword])
+
+/*
+why we dont use var more because its has a scope problem scope means {
+}
+this type of bracket..........
+*/
+
+
+
+let username;
+//if we dont define the variable its when be print it its so undefined
+
+console.log(username)
